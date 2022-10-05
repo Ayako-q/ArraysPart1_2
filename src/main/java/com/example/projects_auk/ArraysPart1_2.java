f02ee7a35593cd1ecd0152025e3527036a63b87b
Ayako-q <ischenkalex@gmail.com>
2022-10-05 13:38:43 +0300

    update

--- src/main/java/com/example/projects_auk/ArraysPart1_2.java
+++ src/main/java/com/example/projects_auk/ArraysPart1_2.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class ArraysPart1_2 {
-    public static void main(String[] args)
-    {
-        while(inGame())
-        {
+    public static void main(String[] args) {
+        printNumbers(5);
+        while (inGame()) {
             System.out.println("Hi! Please, enter number of elements you want:");
             int elementsNum = userIn();
             System.out.println("Do you want to fill an array by yourself? (1 = Yes, 2 = No)");
             int generateHimself = userIn();
-            if (generateHimself == 1){
+            if (generateHimself == 1) {
                 bubbleSort(inputArray(elementsNum)); //User fills an array by him/herself
             } else {
                 bubbleSort(randArray(elementsNum)); //Array fills by random values
@@ -21,21 +20,18 @@ public class ArraysPart1_2 {
     }
 
     //1.1 Array Output
-    public static void arrayOutput(int[] inputArray)
-    {
+    public static void arrayOutput(int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++)
             System.out.print(inputArray[i] + " ");
         System.out.println();
     }
 
     //1.2 Array of given length with random values
-    public static int[] randArray(int arrSize)
-    {
+    public static int[] randArray(int arrSize) {
         int[] array = {};
         array = new int[arrSize]; //size of array from users input
         Random ran = new Random();
-        for (int i = 0; i < array.length; i++)
-        {
+        for (int i = 0; i < array.length; i++) {
             array[i] = ran.nextInt(100); //fill array with numbers below 100
         }
         System.out.println("You array looks like this:\n");
@@ -44,12 +40,10 @@ public class ArraysPart1_2 {
     }
 
     //1.3 Array of given length with given values
-    public static int[] inputArray(int arrSize)
-    {
+    public static int[] inputArray(int arrSize) {
         int[] array = new int[arrSize];
         int arrlength = array.length;
-        for (int i = 0; i < arrlength; i++)
-        {
+        for (int i = 0; i < arrlength; i++) {
             System.out.println("Enter value that you want to add to your array" +
                     "\n you've got " + (arrlength - i) + " elements left:");
             array[i] = userIn();
@@ -60,8 +54,7 @@ public class ArraysPart1_2 {
     }
 
     //1.4 Bubble sort
-    public static void bubbleSort(int[] inputArray)
-    {
+    public static void bubbleSort(int[] inputArray) {
         int tempVal = 0;
         boolean changed = false;
         for (int i = 0; i < inputArray.length - 1; i++)
@@ -83,25 +76,32 @@ public class ArraysPart1_2 {
         arrayOutput(inputArray);
     }
 
-    public static int userIn()
-    {
+    public static int userIn() {
         Scanner in = new Scanner(System.in);
         return in.nextInt();
     }
 
-    public static boolean inGame ()
-    {
+    public static boolean inGame() {
         System.out.println("Do you want to continue? (1 = Yes, 2 = No) :");
         int wannaplay = userIn();
-        if (wannaplay == 1) {
+        if (wannaplay == 1)
+        {
             System.out.println("Okay, lets do it one more time");
             return true;
         }
-        else if (wannaplay == 2){
+        else if (wannaplay == 2)
+        {
             System.out.println("Okay, thank you for your time, bye!");
             return false;
         }
         System.out.println("That is something different from 1 or 2");
         return false;
+    }
+
+    public static void printNumbers(int i) {
+        if (i == 1)
+            return;
+        System.out.print(" " + i);
+        printNumbers(i - 1);
     }
 }
